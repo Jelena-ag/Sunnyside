4ff498b56cd52a6cb03dd727984075340fa42a28
Jelena <jelena.agiejewa@gmail.com>
2024-01-10 14:10:03 +0100

    SA-menu-added nav and header

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.nav__burger');
+const activeElement = document.querySelectorAll('.show');
+
+burger.addEventListener('click', function () {
+	for (let i = 0; i < activeElement.length; i++) {
+		activeElement[i].classList.toggle('show');
+	}
+});
