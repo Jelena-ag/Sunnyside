5907698568b86682660925a08b7bce044def3559
Jelena <jelena.agiejewa@gmail.com>
2024-01-12 11:35:40 +0100

    SA-menu fixed

--- js/script.js
+++ js/script.js
@@ -5,4 +5,5 @@ burger.addEventListener('click', function () {
 	for (let i = 0; i < activeElement.length; i++) {
 		activeElement[i].classList.toggle('show');
 	}
+	burger.classList.toggle('burger__rotate');
 });
